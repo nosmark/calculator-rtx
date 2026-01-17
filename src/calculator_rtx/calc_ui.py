1f21c6e6d4b0193bb0e66c7ca72f9244d5839186
nosmark <mike2202oooo@gmail.com>
2026-01-17 19:44:46 +0300

    calc v3.0

--- src/calculator_rtx/calc_ui.py
+++ src/calculator_rtx/calc_ui.py
@@ -1,15 +1,41 @@
 import calc_logic
+import os
+import calc_history
+GREEN = "\033[1;32m"
+
+def clean_screen():
+    os.system("clear")
+
+clean_screen()
+calc_history.history_del()
 
 while True:
-    print("---RTX Calculator---\n1)choose options: + -  * /  ** %\nor\n2)exit")
+
+    print(f"{GREEN}---RTX Calculator---\n1)choose options: + -  * /  ** %\nor\n2)exit\nor\n3)see history")
 
     choise = input("choise: ")
     if choise == "exit" or choise == "2":
+        calc_history.history_del()
+        clean_screen()
         break
+
+    elif choise == "3" or choise == "see history":
+        clean_screen()
+        calc_history.history_watch()
+        cont1 = input("press enter to continue...")
+
     else:
         first_num = input("enter first num: ")
         sec_num = input("enter sec num: ")
+        print("")
+        clean_screen()
+
         try:
-            print(calc_logic.calculator(choise, int(first_num), int(sec_num)), "\n")
+            r = calc_logic.calculator(choise, int(first_num), int(sec_num))
+            print("result:", r, "\n")
+            calc_history.history_add(first_num, choise, sec_num, r)
         except:
-            print("mewmewmew")
+            print("mewmewmew\n")
+
+        cont2 = input("press enter to continue...")
+        clean_screen()
